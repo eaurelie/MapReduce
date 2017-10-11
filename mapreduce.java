9e15e04b1df6ff1e5f3db6ea5e3f0d3ee0b992bf
eaurelie <aurelie.ea@gmail.com>
2017-10-11 12:51:40 +0200

    pivot
    pivot

--- mapreduce.java
+++ mapreduce.java
@@ -1,22 +1,50 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+public class Pivot {
 
-public static class Map extends Mapper<Object, Text, Text, Text> {
+public static class Map extends Mapper<Object, Text,IntWritable , Text> {
        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
           String[] tableaux = value.toString().split(",");
-          for (int i=1; i<tableaux.length ; i++)
+ for (int i=0; i<tableaux.length ; i++)
           {
-            context.write(new Text(i), new Text(tableaux[i]));
+   context.write(new  IntWritable(i) , new Text(tableaux[i]));
           }
-
        }
 }
-public static class Reduce extends Reducer<Text, Text, Text, Text {
-       public void reduce(Text key, Iterable<Text> values,Context context) throws IOException,InterruptedException {
+public static class Reduce extends Reducer<IntWritable,Text,IntWritable,Text> {
 
-              for (IntWritable val : values) {
-                  String row += val.toString() + ',';
-              }
-              String res = row.substring(0, row.length() - 1);
-              context.write(key, new Text(res));
+    public void reduce(IntWritable key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException{
+
+String row = "";
+for (Text val : values)
+    {
+           row += val.toString() + ",";
      }
+           String res = row.substring(0, row.length - 1);
+            context.write(key, new Text(res));
+   }
+}
+
+public static void main(String[] args) throws Exception {
+ Configuration conf = new Configuration();
+ Job job = Job.getInstance(conf, "pivot");
+ job.setJarByClass(Pivot.class);
+   job.setMapperClass(Map.class);
+ job.setCombinerClass(Reduce.class);
+ job.setReducerClass(Reduce.class);
+     job.setOutputKeyClass(IntWritable.class);
+  job.setOutputValueClass(Text.class);
+FileInputFormat.addInputPath(job, new Path(args[0]));
+ FileOutputFormat.setOutputPath(job, new Path(args[1]));
+ System.exit(job.waitForCompletion(true) ? 0 : 1);
+}
 }
